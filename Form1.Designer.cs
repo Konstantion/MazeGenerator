0e3fc7aacb90f5a965530a1799ea83dc2124086f
Konstantion <yagudinkostya@gmail.com>
2022-10-29 00:20:32 +0300

    Создал снипет гуи, разобралсяс выпадающим списком

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,9 @@
 ﻿
+using MazeGenerator.src.algorithms;
+using System;
+using System.Collections.Generic;
+
+
 namespace MazeGenerator
 {
     partial class Form1
@@ -31,11 +36,21 @@ namespace MazeGenerator
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.comboBoxAlgorithm = new System.Windows.Forms.ComboBox();
+            this.labelMazeWidth = new System.Windows.Forms.Label();
+            this.numericUpDownMazeSize = new System.Windows.Forms.NumericUpDown();
+            this.labelAlgorithmName = new System.Windows.Forms.Label();
+            this.labelFrames = new System.Windows.Forms.Label();
+            this.numericUpDownFrames = new System.Windows.Forms.NumericUpDown();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.labelStop = new System.Windows.Forms.Label();
+            this.labelReset = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMazeSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFrames)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -51,14 +66,23 @@ namespace MazeGenerator
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.listView1, 3, 0);
-            this.tableLayoutPanel1.Controls.Add(this.listBox1, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.checkedListBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelStart, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelFrames, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelMazeWidth, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDownMazeSize, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelAlgorithmName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxAlgorithm, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDownFrames, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelStop, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelReset, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonStart, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 4, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(1, -3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 200F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 200F));
@@ -78,30 +102,152 @@ namespace MazeGenerator
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // listView1
+            // comboBoxAlgorithm
             // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(603, 3);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(121, 94);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.comboBoxAlgorithm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxAlgorithm.DisplayMember = "Name";
+            this.comboBoxAlgorithm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAlgorithm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxAlgorithm.FormattingEnabled = true;
+            this.comboBoxAlgorithm.Location = new System.Drawing.Point(203, 61);
+            this.comboBoxAlgorithm.Name = "comboBoxAlgorithm";
+            this.comboBoxAlgorithm.Size = new System.Drawing.Size(194, 28);
+            this.comboBoxAlgorithm.TabIndex = 1;
+            this.comboBoxAlgorithm.ValueMember = "Id";
             // 
-            // listBox1
+            // labelMazeWidth
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(403, 3);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 82);
-            this.listBox1.TabIndex = 2;
+            this.labelMazeWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelMazeWidth.AutoSize = true;
+            this.labelMazeWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMazeWidth.Location = new System.Drawing.Point(3, 0);
+            this.labelMazeWidth.Name = "labelMazeWidth";
+            this.labelMazeWidth.Size = new System.Drawing.Size(194, 50);
+            this.labelMazeWidth.TabIndex = 2;
+            this.labelMazeWidth.Text = "Imput size ";
+            this.labelMazeWidth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // checkedListBox1
+            // numericUpDownMazeSize
             // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(203, 3);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
-            this.checkedListBox1.TabIndex = 3;
+            this.numericUpDownMazeSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownMazeSize.AutoSize = true;
+            this.numericUpDownMazeSize.Location = new System.Drawing.Point(3, 65);
+            this.numericUpDownMazeSize.Name = "numericUpDownMazeSize";
+            this.numericUpDownMazeSize.Size = new System.Drawing.Size(194, 20);
+            this.numericUpDownMazeSize.TabIndex = 3;
+            // 
+            // labelAlgorithmName
+            // 
+            this.labelAlgorithmName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelAlgorithmName.AutoSize = true;
+            this.labelAlgorithmName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAlgorithmName.Location = new System.Drawing.Point(203, 0);
+            this.labelAlgorithmName.Name = "labelAlgorithmName";
+            this.labelAlgorithmName.Size = new System.Drawing.Size(194, 50);
+            this.labelAlgorithmName.TabIndex = 4;
+            this.labelAlgorithmName.Text = "Algorithm name";
+            this.labelAlgorithmName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelAlgorithmName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelFrames
+            // 
+            this.labelFrames.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFrames.AutoSize = true;
+            this.labelFrames.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFrames.Location = new System.Drawing.Point(403, 0);
+            this.labelFrames.Name = "labelFrames";
+            this.labelFrames.Size = new System.Drawing.Size(194, 50);
+            this.labelFrames.TabIndex = 5;
+            this.labelFrames.Text = "FPS";
+            this.labelFrames.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDownFrames
+            // 
+            this.numericUpDownFrames.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownFrames.AutoSize = true;
+            this.numericUpDownFrames.Location = new System.Drawing.Point(403, 65);
+            this.numericUpDownFrames.Name = "numericUpDownFrames";
+            this.numericUpDownFrames.Size = new System.Drawing.Size(194, 20);
+            this.numericUpDownFrames.TabIndex = 6;
+            this.numericUpDownFrames.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // labelStart
+            // 
+            this.labelStart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelStart.AutoSize = true;
+            this.labelStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelStart.Location = new System.Drawing.Point(603, 0);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(194, 50);
+            this.labelStart.TabIndex = 7;
+            this.labelStart.Text = "Start";
+            this.labelStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelStop
+            // 
+            this.labelStop.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelStop.AutoSize = true;
+            this.labelStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelStop.Location = new System.Drawing.Point(803, 0);
+            this.labelStop.Name = "labelStop";
+            this.labelStop.Size = new System.Drawing.Size(194, 50);
+            this.labelStop.TabIndex = 7;
+            this.labelStop.Text = "Stop";
+            this.labelStop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelReset
+            // 
+            this.labelReset.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelReset.AutoSize = true;
+            this.labelReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelReset.Location = new System.Drawing.Point(1003, 0);
+            this.labelReset.Name = "labelReset";
+            this.labelReset.Size = new System.Drawing.Size(194, 50);
+            this.labelReset.TabIndex = 7;
+            this.labelReset.Text = "Reset";
+            this.labelReset.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(603, 53);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(194, 44);
+            this.buttonStart.TabIndex = 8;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(803, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 44);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -112,7 +258,10 @@ namespace MazeGenerator
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMazeSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFrames)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -121,9 +270,17 @@ namespace MazeGenerator
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox comboBoxAlgorithm;
+        private System.Windows.Forms.Label labelMazeWidth;
+        private System.Windows.Forms.NumericUpDown numericUpDownMazeSize;
+        private System.Windows.Forms.Label labelAlgorithmName;
+        private System.Windows.Forms.Label labelFrames;
+        private System.Windows.Forms.NumericUpDown numericUpDownFrames;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.Label labelStop;
+        private System.Windows.Forms.Label labelReset;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button button1;
     }
 }
 
